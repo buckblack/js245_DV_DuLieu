ad79eed6760e53ca8108d76c89087853cc93845f
buckblack <42344854+buckblack@users.noreply.github.com>
2019-03-17 12:26:16 +0700

    Update dv_du_lieu.js

--- dich_vu/dv_du_lieu.js
+++ dich_vu/dv_du_lieu.js
@@ -11,6 +11,7 @@ var MongoClient = mongodb.MongoClient;
 var url = 'mongodb://admin:minhtien96@ds259154.mlab.com:59154/js245_doan';
 var ds_ban;
 var cl_ban;
+var cl_hoadon;
 var db;
 MongoClient.connect(url, function (err, client) {
   if (err) {
@@ -22,16 +23,24 @@ MongoClient.connect(url, function (err, client) {
     db = client.db(csdl)
     // Chọn Collection
     cl_ban = db.collection("ban")
+    cl_hoadon = db.collection("hoa_don")
   }
 });
 var Dich_vu = NodeJs_Dich_vu.createServer((Yeu_cau, Dap_ung) => {
   var Chuoi_Nhan = ""
   var Dia_chi_Xu_ly = Yeu_cau.url.replace("/", "")
+  var thamso2;
   Yeu_cau.on('data', (chunk) => {
     Chuoi_Nhan += chunk
   }) //data: nhận dl từ client(như ajax)
   Yeu_cau.on('end', () => { //end: trả kq lại cho client
-
+    if (Dia_chi_Xu_ly.indexOf('/') >= 0) {
+      var giatri = Dia_chi_Xu_ly.split('/')
+      Dia_chi_Xu_ly = giatri[0];
+      tham_so_phu = giatri[1];
+      var thamso2 = Xu_ly_Tham_so.parse(tham_so_phu.replace("?", ""))
+      var laythamsao = thamso2.thamso
+    }
     var Tham_so = Xu_ly_Tham_so.parse(Dia_chi_Xu_ly.replace("?", ""))
     var Ma_so_Xu_ly = Tham_so.Ma_so_Xu_ly
     var Chuoi_Kq = ""
@@ -67,11 +76,10 @@ var Dich_vu = NodeJs_Dich_vu.createServer((Yeu_cau, Dap_ung) => {
         Dap_ung.end(Chuoi_Kq);
       })
     } else if (Ma_so_Xu_ly == "danh_sach_ban") {
-      ds_ban = cl_ban.find({}).toArray((err, req) => {
+      ds_ban = cl_hoadon.find({}).toArray((err, req) => {
         if (err)
           console.log(err);
         else {
-          console.log(req);
           var Doi_tuong_Kq = req
           Chuoi_Kq = JSON.stringify(Doi_tuong_Kq)
           Dap_ung.setHeader("Access-Control-Allow-Origin", '*')
@@ -82,6 +90,60 @@ var Dich_vu = NodeJs_Dich_vu.createServer((Yeu_cau, Dap_ung) => {
         }
       })
 
+    }else if (Ma_so_Xu_ly == "hoa_don_moi") {
+      ds_ban = cl_hoadon.find({}).toArray((err, req) => {
+        if (err)
+          console.log(err);
+        else {
+          var Doi_tuong_Kq =Number(req[req.length-1].ma_hd)+1
+          Chuoi_Kq = JSON.stringify(Doi_tuong_Kq)
+          Dap_ung.setHeader("Access-Control-Allow-Origin", '*')
+          Dap_ung.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+          Dap_ung.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+          Dap_ung.setHeader('Access-Control-Allow-Credentials', true);
+          Dap_ung.end(Chuoi_Kq);
+        }
+      })
+    } else if (Ma_so_Xu_ly == "tim_hoa_don") {
+      try {
+        var dk = {
+          "ma_hd": Number(laythamsao)
+        }
+        cl_hoadon.find(dk,(err, req) => {
+          if (err)
+            console.log(err);
+          else {
+            var Doi_tuong_Kq = req
+            Chuoi_Kq = JSON.stringify(Doi_tuong_Kq)
+            Dap_ung.setHeader("Access-Control-Allow-Origin", '*')
+            Dap_ung.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+            Dap_ung.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+            Dap_ung.setHeader('Access-Control-Allow-Credentials', true);
+            Dap_ung.end(Chuoi_Kq);
+          }
+        })
+      } catch (error) {
+      }
+    }else if (Ma_so_Xu_ly == "tim_ban") {
+      try {
+        var dk = {
+          "ma_ban": Number(laythamsao)
+        }
+        cl_ban.findOne(dk,(err, req) => {
+          if (err)
+            console.log(err);
+          else {
+            var Doi_tuong_Kq = req
+            Chuoi_Kq = JSON.stringify(Doi_tuong_Kq)
+            Dap_ung.setHeader("Access-Control-Allow-Origin", '*')
+            Dap_ung.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+            Dap_ung.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+            Dap_ung.setHeader('Access-Control-Allow-Credentials', true);
+            Dap_ung.end(Chuoi_Kq);
+          }
+        })
+      } catch (error) {
+      }
     } else {
       Chuoi_Kq = Luu_tru.Doc_Thong_tin_Dich_vu()
       Dap_ung.setHeader("Access-Control-Allow-Origin", '*')
